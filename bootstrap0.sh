2b10a6481205ce6b5244e7d87a6da87bb2d9c510
atrn0 <14242642+atrn0@users.noreply.github.com>
2022-02-16 22:00:57 +0900

    Fix partition path

--- bootstrap0.sh
+++ bootstrap0.sh
@@ -9,13 +9,13 @@ parted /dev/$NIXBLOCKDEVICE -- mkpart primary 512MiB -8GiB
 parted /dev/$NIXBLOCKDEVICE -- mkpart primary linux-swap -8GiB 100%
 parted /dev/$NIXBLOCKDEVICE -- mkpart ESP fat32 1MiB 512MiB
 parted /dev/$NIXBLOCKDEVICE -- set 3 esp on
-mkfs.ext4 -L nixos /dev/${NIXBLOCKDEVICE}1
-mkswap -L swap /dev/${NIXBLOCKDEVICE}2
-mkfs.fat -F 32 -n boot /dev/${NIXBLOCKDEVICE}3
+mkfs.ext4 -L nixos /dev/${NIXBLOCKDEVICE}p1
+mkswap -L swap /dev/${NIXBLOCKDEVICE}p2
+mkfs.fat -F 32 -n boot /dev/${NIXBLOCKDEVICE}p3
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
 mount /dev/disk/by-label/boot /mnt/boot
-swapon /dev/${NIXBLOCKDEVICE}2
+swapon /dev/${NIXBLOCKDEVICE}p2
 nixos-generate-config --root /mnt
 curl --location --output /mnt/etc/nixos/configuration.nix https://raw.githubusercontent.com/atrn0/nixos-config/main/configuration.nix
 nixos-install --no-root-passwd
