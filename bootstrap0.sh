de39e6f746e3b6e702c0eaf87a5903b60176a06a
atrn0 <14242642+atrn0@users.noreply.github.com>
2022-02-16 21:52:46 +0900

    Update bootstrap0.sh

--- bootstrap0.sh
+++ bootstrap0.sh
@@ -2,18 +2,20 @@
 
 set -euxo pipefail
 
-parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart primary 512MiB -8GiB
-parted /dev/sda -- mkpart primary linux-swap -8GiB 100%
-parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB
-parted /dev/sda -- set 3 esp on
-mkfs.ext4 -L nixos /dev/sda1
-mkswap -L swap /dev/sda2
-mkfs.fat -F 32 -n boot /dev/sda3
+: "${NIXBLOCKDEVICE:=nvme0}"
+
+parted /dev/$NIXBLOCKDEVICE -- mklabel gpt
+parted /dev/$NIXBLOCKDEVICE -- mkpart primary 512MiB -8GiB
+parted /dev/$NIXBLOCKDEVICE -- mkpart primary linux-swap -8GiB 100%
+parted /dev/$NIXBLOCKDEVICE -- mkpart ESP fat32 1MiB 512MiB
+parted /dev/$NIXBLOCKDEVICE -- set 3 esp on
+mkfs.ext4 -L nixos /dev/${NIXBLOCKDEVICE}1
+mkswap -L swap /dev/${NIXBLOCKDEVICE}2
+mkfs.fat -F 32 -n boot /dev/${NIXBLOCKDEVICE}3
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
 mount /dev/disk/by-label/boot /mnt/boot
-swapon /dev/sda2
+swapon /dev/${NIXBLOCKDEVICE}2
 nixos-generate-config --root /mnt
 curl --location --output /mnt/etc/nixos/configuration.nix https://raw.githubusercontent.com/atrn0/nixos-config/main/configuration.nix
 nixos-install --no-root-passwd
